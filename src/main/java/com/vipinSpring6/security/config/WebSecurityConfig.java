b60a112797aea16934b81925532aba690ae7143b
vipinshivhare <vipinshivhare804@gmail.com>
2025-08-21 12:37:10 +0530

    Config

---
+++ src/main/java/com/vipinSpring6/security/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.vipinSpring6.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(
+                        request -> request.anyRequest().authenticated()
+                );
+
+        return httpSecurity.build();
+    }
+
+}
---
+++ src/main/java/com/vipinSpring6/security/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.vipinSpring6.security.controller;
+
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private record Product(Integer productId, String productName, double price){
+
+    }
+
+    List<Product> products = new ArrayList<>(
+            List.of(
+                    new Product(1, "Iphone", 999),
+                    new Product(2, "Samsung", 999),
+                    new Product(3, "Oppo", 999)
+            )
+    );
+
+    @GetMapping
+    public List<Product> getProducts(){
+        return products;
+    }
+
+    @PostMapping
+    public Product saveproduct(@RequestBody Product product){
+        products.add(product);
+        return product;
+    }
+
+
+
+}
